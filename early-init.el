146c9fbf0f784917ec99cdf52303cbe4e198abfd
thecomplication <dawilltwd@gmail.com>
2024-01-14 13:05:45 +0000

    initial commit

---
+++ early-init.el
@@ -0,0 +1,4 @@
+(setq package-enable-at-startup nil)
+
+(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/libgccjit/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin20/10.2.0")
+
